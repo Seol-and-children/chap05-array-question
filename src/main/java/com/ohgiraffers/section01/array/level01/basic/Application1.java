1413f5fe91fffcb714a6bc76d96ec275c9bfaf68
phj9745 <117703172+phj9745@users.noreply.github.com>
2023-05-08 15:40:48 +0900

    basic 커밋&푸쉬

--- src/main/java/com/ohgiraffers/section01/array/level01/basic/Application1.java
+++ src/main/java/com/ohgiraffers/section01/array/level01/basic/Application1.java
@@ -8,6 +8,12 @@ public class Application1 {
 		 * 각 인덱스에 차례대로 1부터 10까지 값을 넣고 출력하세요
 		 * */
 
+		int[] arr = new int[10];
+
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
+			System.out.println(arr[i]);
+		}
 	}
 
 }
